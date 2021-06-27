ce30c3000f01e7c2381f596656bc7f7f78401a04
boycot <boycot2017@163.com>
2021-06-27 23:06:09 +0800

    处理首页显示问题

--- src/views/components/GridList.vue
+++ src/views/components/GridList.vue
@@ -1,6 +1,6 @@
 <template>
 <li class="grid-list-item fl clearfix js-list-detail"
-v-if="item.img1v1Url || item.coverImgUrl || item.coverUrl || item.cover || item.sPicUrl || item.picUrl"
+v-if="(item.list && item.list.length) || item.img1v1Url || item.coverImgUrl || item.coverUrl || item.cover || item.sPicUrl || item.picUrl"
 :title="item.rcmdtext || item.name" :class="`type-${type || item.type} ftype-${item.ftype}`" data-url="{{item.mp3Url}}">
     <ul v-if="item.list" class="grid-list js-child-list fl">
         <li class="grid-list-item clearfix js-list-detail" :class="`type-${child.type} ftype-${child.ftype}`" :title="child.name" v-for="(child, cindex) in item.list" :key="cindex" data-url="{{child.mp3Url}}">
