6f25b0779b61213e1091eeffc892f2516a480689
boycot <boycot2017@163.com>
2021-07-01 00:34:24 +0800

    去除多余代码

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,15 @@
 import toTop from './toTop'
-import colorPicker from './colorPicker'
-import lyric from './lyric'
-import docker from './docker'
-import deskTop from './deskTop'
-import contextMenu from './contextmenu'
+// import colorPicker from './colorPicker'
+// import lyric from './lyric'
+// import docker from './docker'
+// import deskTop from './deskTop'
+// import contextMenu from './contextmenu'
 
 export default {
-    toTop,
-    colorPicker,
-    lyric,
-    docker,
-    deskTop,
-    contextMenu
+    toTop
+    // colorPicker,
+    // lyric,
+    // docker,
+    // deskTop,
+    // contextMenu
 }
