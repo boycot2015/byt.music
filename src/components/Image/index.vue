7ec5f02e8ea827123ac5ec6b06c12eceeea115d8
zhangchunhui <boycot2017@163.com>
2025-07-17 19:30:08 +0800

    优化主题颜色变量，调整背景色透明度，新增自定义图片组件

---
+++ src/components/Image/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-image v-bind="$attrs">
+    <template #error>
+      <el-icon>
+        <icon-music />
+      </el-icon>
+    </template>
+    <template #placeholder>
+      <el-icon>
+        <icon-music />
+      </el-icon>
+    </template>
+  </el-image>
+</template>
